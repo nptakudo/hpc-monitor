0c91bbc478fc3d76da3e1f6f33f0015972d0b393
nptakudo <thinh.nguyenph@hcmut.edu.vn>
2025-02-23 10:25:50 +0700

    nothing

--- push/test_push_data.py
+++ push/test_push_data.py
@@ -18,14 +18,14 @@ bucket="test"
 # Write the data
 write_api = client.write_api(write_options=SYNCHRONOUS)
    
-for value in range(5):
-  point = (
-    Point("measurement1")
-    .tag("tagname1", "tagvalue1")
-    .field("field1", value)
-  )
-  write_api.write(bucket=bucket, org="docs", record=point)
-  time.sleep(1) # separate points by 1 second
+# for value in range(5):
+#   point = (
+#     Point("measurement1")
+#     .tag("tagname1", "tagvalue1")
+#     .field("field1", value)
+#   )
+#   write_api.write(bucket=bucket, org="docs", record=point)
+#   time.sleep(1) # separate points by 1 second
 
 
 # Query the data
